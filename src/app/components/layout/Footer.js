659bcc8fb2291e03b61a1be8b9be6eb44838de6d
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2024-02-01 22:21:24 +0545

    updated footer

--- src/app/components/layout/Footer.js
+++ src/app/components/layout/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="bg-white ">
+        <footer className="bg-gray-100 ">
             <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0">
